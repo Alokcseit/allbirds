afd1d944851c380600453fc8a4506d8a6ca746dc
Alokcsit <swainalokkumar25@gmail.com>
2024-07-20 00:32:03 +0530

    very initial commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res)=>
+{
+    res.send('Hello, World!');
+})
+
+const port=4000
+
+app.listen(port, ()=>
+{
+    console.log(`Server is running on port ${port}`);
+})
