19af5aabf73eae90d482b6f1fb069d86e0176d97
Alokcsit <swainalokkumar25@gmail.com>
2024-07-21 00:24:47 +0530

    very first commit

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,14 @@
 import express from 'express';
-
+import * as dotenv from "dotenv";
 const app = express();
-
+dotenv.config({
+    path: "./.env",
+  });
 app.get('/', (req, res)=>
 {
     res.send('Hello, World!');
 })
-
-const port=4000
-
-app.listen(port, ()=>
+app.listen(process.env.PORT, ()=>
 {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
 })
